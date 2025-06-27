e0c4bc5d0182bf26a6823b16e108385cf2243961
Surendhar G <surendharg123@gmail.com>
2025-06-27 09:01:01 +0530

    bastion and ALB SGs added

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -3,9 +3,56 @@ module "frontend"{
     source          = "git::https://github.com/gsurendhar/terraform-aws-security-group-module.git?ref=master"
     project         = var.project
     environment     = var.environment
-    sg_name         = var.sg_name
+    sg_name         = "${local.Name}-${var.sg_name}"
     sg_description  = var.sg_description
     # vpc_id          = data.aws_ssm_parameter.vpc_id.value
     vpc_id          = local.vpc_id
 }
 
+#  creating SG for bastion host to connection from laptops
+module "bastion"{
+    source          = "git::https://github.com/gsurendhar/terraform-aws-security-group-module.git?ref=master"
+    project         = var.project
+    environment     = var.environment
+    sg_name         = "${local.Name}-bastion"
+    sg_description  = "created sg for bastion instances"
+    vpc_id          = local.vpc_id
+}
+
+module "backend_alb"{
+    source          = "git::https://github.com/gsurendhar/terraform-aws-security-group-module.git?ref=master"
+    project         = var.project
+    environment     = var.environment
+    sg_name         = "${local.Name}-backend_alb"
+    sg_description  = "created sg for backend load balancer"
+    vpc_id          = local.vpc_id
+}
+
+
+
+
+
+
+
+
+
+# bastion accepting connections from my laptop
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion.sg_id
+}
+
+# ALB accepting connections from bastion host on port no 80
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type                      = "ingress"
+  from_port                 = 80
+  to_port                   = 80
+  protocol                  = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+  source_security_group_id  = module.bastion.sg_id
+  security_group_id         = module.backend_alb.sg_id
+}
