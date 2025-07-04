f7d2c18c23589c0dc2fce64bf9542e86384fb4e3
gsurendhar <surendharg123@gmail.com>
2025-07-04 17:11:49 +0530

    frontend_alb added

---
+++ 70-frontend-alb/locals.tf
@@ -0,0 +1,17 @@
+locals {
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  private_subnet_ids  = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
+  frontend_alb_sg_id  = data.aws_ssm_parameter.frontend_alb_sg_id.value
+  acm_certificate_arn = data.acm_certificate_arn.value
+
+  common_tags = {
+      Project      = var.project
+      Environment  = var.environment
+      Terraform    = true
+  }
+
+  Name = "${var.project}-${var.environment}"
+
+  zone_name = data.aws_route53_zone.selected.name
+  zone_id = data.aws_route53_zone.selected.zone_id
+}
