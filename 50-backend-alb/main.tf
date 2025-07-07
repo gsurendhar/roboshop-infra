ec9670435a2dab861ec78db4524f096fd42805cd
Surendhar G <surendharg123@gmail.com>
2025-07-07 09:17:04 +0530

    ALB name updated

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -3,7 +3,7 @@ module "backend_alb" {
     source                  = "terraform-aws-modules/alb/aws"
     version                 = "9.16.0"
     internal                = true 
-    name                    = "${local.Name}-backend_alb"
+    name                    = "${local.Name}-backend-alb"
     vpc_id                  = local.vpc_id
     subnets                 = local.private_subnet_ids
     create_security_group   = false
