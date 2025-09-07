f62c3902337f229ebc3144b7e42e86e9cab9a309
Surendhar G <surendharg123@gmail.com>
2025-09-07 12:18:37 +0530

    Env variable is added

--- 90-componets/main.tf
+++ 90-componets/main.tf
@@ -1,7 +1,7 @@
 module "component" {
     for_each        = var.component
-    # source          = "git::https://github.com/gsurendhar/terraform-aws-roboshop-dev-module.git?ref=main"
-    source          = "../../terraform-aws-roboshop-dev-module"
+    source          = "git::https://github.com/gsurendhar/terraform-aws-roboshop-dev-module.git?ref=main"
+    # source          = "../../terraform-aws-roboshop-dev-module"
     component       = each.key 
     rule_priority   = each.value.rule_priority
     environment     = var.environment
