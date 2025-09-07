4ac050435d5c45ce3a8da0e1cf88e97f3160198b
Surendhar G <surendharg123@gmail.com>
2025-09-07 11:50:31 +0530

    Env variable is added

--- 90-componets/main.tf
+++ 90-componets/main.tf
@@ -3,4 +3,5 @@ module "component" {
     source          = "git::https://github.com/gsurendhar/terraform-aws-roboshop-dev-module.git?ref=main"
     component       = each.key 
     rule_priority   = each.value.rule_priority
+    environment     = var.environment
 }
