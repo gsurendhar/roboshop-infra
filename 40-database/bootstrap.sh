0ac7c17ab1a6882d371de7a206e511b6ead4b733
Surendhar G <surendharg123@gmail.com>
2025-09-07 22:21:54 +0530

    Databases bootstrap file is updated

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
 component=$1
+env=$2
 dnf install ansible -y
-ansible-pull -U https://github.com/gsurendhar/ansible-roboshop-by-roles.git -e component=$1 main.yaml
+ansible-pull -U https://github.com/gsurendhar/ansible-roboshop-by-roles-env.git -e component=$1 -e env=$2 main.yaml
