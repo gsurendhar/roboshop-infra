672ba48e39594f683ccd9263cc5835b6cc778042
Surendhar G <surendharg123@gmail.com>
2025-09-07 12:22:04 +0530

    Env variable is added

--- 90-componets/bootstrap.sh
+++ 90-componets/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-env=$1
-component=$2
+component=$1
+env=$2
 dnf install ansible -y
-ansible-pull -U https://github.com/gsurendhar/ansible-roboshop-by-roles-env.git  -e env=$1 -e component=$2 main.yaml
+ansible-pull -U https://github.com/gsurendhar/ansible-roboshop-by-roles-env.git -e component=$1 -e env=$2 main.yaml
